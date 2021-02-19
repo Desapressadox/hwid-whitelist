e6047d5fc87b3ed07707d3772e58e5edc3df9993
Desapressado <59629875+Desapressadox@users.noreply.github.com>
2021-02-19 12:27:30 -0300

    Add files via upload

---
+++ hwid.py
@@ -0,0 +1,23 @@
+import os
+import requests
+import time
+import subprocess
+import urllib3
+
+urllib3.disable_warnings() # desabilita o log dos requests
+
+hwid = str(subprocess.check_output('wmic csproduct get uuid')).split('\\r\\n')[1].strip('\\r').strip() # pega o hwid
+request = requests.get('https://pastebin.com/raw/LINK', verify=False) # entra no link onde armazena os hwid's
+
+def menu():
+    os.system("cls")
+    print("oi")
+
+if hwid in request.text: # checa se o hwid é válido
+   print("HWID autorizado, redirecionando...")
+   time.sleep(2.4)
+   menu()
+else: # checa se o hwid é inválido
+   print("HWID não autorizado")
+   time.sleep(2.4)
+   quit()
